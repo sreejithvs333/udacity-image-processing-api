fe9ce61b8742c5a69241b251e3f96a5246a6e8d1
sree <sreejithvs333@gmail.com>
2023-04-06 09:12:16 +0530

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ app.get('/', (req, res)=>{
 });
 
 app.listen(port, ()=> {
-    console.log(`The server starter at http://localhost:${port}`);
+    console.log(`The server started at http://localhost:${port}`);
 });
