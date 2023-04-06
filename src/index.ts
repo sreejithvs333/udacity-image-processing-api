2ba5c0b33f0530887a0d6170e400478c4608ddb2
sree <sreejithvs333@gmail.com>
2023-04-06 09:20:48 +0530

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-
+import routes from './routes/index';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res)=>{
-    res.send("Server working");
-});
+app.use('/api',routes);
 
 app.listen(port, ()=> {
     console.log(`The server started at http://localhost:${port}`);
