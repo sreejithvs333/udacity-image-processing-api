eb9f1c6ec625511ab7cb380d0294247732e24f42
sree <sreejithvs333@gmail.com>
2023-04-08 10:42:16 +0530

    add jasmine to project

---
+++ dist/tests/indexSpec.js
@@ -0,0 +1,5 @@
+"use strict";
+it("sample test", function () {
+    var number = 1;
+    expect(number).toEqual(1);
+});
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,5 @@
+
+it("sample test", () => {
+    let number = 1;
+    expect(number).toEqual(1);
+})
