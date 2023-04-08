7c1c436363610cc7fd7fa2ec308de4b6ab5e5319
sree <sreejithvs333@gmail.com>
2023-04-08 13:20:05 +0530

    fix error: Cannot set headers after they are sent to the client

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -8,10 +8,13 @@ images.get("/", async (req: express.Request, res: express.Response) => {
   const query: CustomTypes.RequestQuery = req.query;
   try {
     const imagePath = await ImageUtils.getImagePath(query);
-    return res.status(200).sendFile(imagePath);
+    res.status(200).sendFile(imagePath);
   } catch (err) {
-    if (err instanceof Error) return res.status(400).send(err.message);
-    return res.status(400).send(String(err));
+    if (err instanceof Error) {
+      res.status(400).send(err.message);
+    } else {
+      res.status(400).send(String(err));
+    }
   }
 });
 
