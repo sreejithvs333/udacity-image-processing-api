6a77f261e800af51750832fb735e2f9d28ed34f1
sree <sreejithvs333@gmail.com>
2023-04-09 08:13:29 +0530

    add test for image resizing

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -3,9 +3,12 @@ import { checkIfExist, getImagePath } from "../utilities/image-utils";
 import { RequestQuery } from "../utilities/types";
 import supertest from "supertest";
 import app from "..";
+import fsPromises from "fs/promises";
+import fs from 'fs';
+import * as sharp from '../utilities/sharp';
+
 describe("unit testing", () => {
   const filePath = path.resolve(__dirname, "../assets/images/full/fjord.jpg");
-
   const querySuccess: RequestQuery = {
     fileName: "palmtunnel",
     width: "200",
@@ -28,6 +31,21 @@ describe("unit testing", () => {
   it("getImagePath(querySuccess) to be truthy", async () => {
     const value = await getImagePath(querySuccess);
     expect(value).toBeTruthy();
+  });
+
+  it("imageResizeAndSave() should create a resized file", async ()=> {
+    const fileThumbPath = path.resolve(__dirname, "../assets/images/thumb/palmtunnelx200x200.jpg")
+    try {
+      // delete the file if already exist
+      await fsPromises.access(path.resolve(__dirname, fileThumbPath));
+      fs.unlinkSync(fileThumbPath);
+      await sharp.imageResizeAndSave(querySuccess);
+    } catch (error) {
+      await sharp.imageResizeAndSave(querySuccess);
+    }
+    
+    expect(await fsPromises.access(fileThumbPath)).not.toBeInstanceOf(Error);
+    
   });
 });
 
